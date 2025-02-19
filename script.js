f2f867a363dea0b1bc61c51ce88a545a89790d1e
rafael <rafaelmoreno141219@gmail.com>
2025-02-19 09:59:58 -0400

    subidas canciones

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const cancion = {
-    _id: '1',
-    author: 'Molotov',
-    title: 'give me the power',
-    duracion: '4:10',
-    audio: {
-        _id: '',
-        url: '',
-        filename: ''
 
-    },
-    image: {
-        _id: '',
-        url: '',
-        filename: ''
-    }
 
-}
+// pedimos la informacion al servidor con axios 
+// el protocolo es http , es un mecanismo para intercambiar datos
+
+axios.get("https://api.institutoalfa.org/api/songs")
+
+// cuando la informacion esta ubicada , se activa el .then
+// la funcion recibe una respuesta que se maneja a conveniencia
+
+.then(function(respuesta) {
+
+    // mostrar por consola
+    console.log(respuesta)
+
+    // ahora guardo en canciones lo que venga de respuesta
+
+    const canciones = respuesta.data.songs
+    
+    // .map mapea , recorre  o descompone las canciones
+    canciones.map(function (cancion){
+
+        // lo que este aqui dentro se va a repetir en toda la lista
+
+        console.log(cancion.title)
+        document.getElementById('container').appendChild(CrearComponenteCancion(cancion))
+    })
+
+})
+
+// crea un elemento cancion y la regresa 
+
 function CrearComponenteCancion(song) {
     const li = document.createElement ('li')
     li.setAttribute('class', 'cancion')
@@ -31,4 +45,4 @@ function CrearComponenteCancion(song) {
 
 }
 
-document.getElementById('container').appendChild(CrearComponenteCancion(cancion))
+// aqui añadimos un componente al reproductor
